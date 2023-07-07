cbac82aea81c874b984da225a44959e273fa1345
Ivan <foxriver660@gmail.com>
2023-07-07 13:22:10 +0300

    feat: completed actor serv.

--- src/actor/actor.module.ts
+++ src/actor/actor.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { ActorController } from './actor.controller';
+import { ActorModel } from './actor.model';
 import { ActorService } from './actor.service';
 
 @Module({
-  providers: [ActorService]
+  providers: [ActorService],
+  controllers: [ActorController],
+  imports: [
+    TypegooseModule.forFeature([
+      {
+        typegooseClass: ActorModel,
+        schemaOptions: { collection: 'actors' },
+      },
+    ]),
+  ],
 })
 export class ActorModule {}
--- src/actor/actor.service.ts
+++ src/actor/actor.service.ts
@@ -1,11 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { ModelType } from '@typegoose/typegoose/lib/types';
 import { InjectModel } from 'nestjs-typegoose';
 import { ActorModel } from './actor.model';
+import { ActorDto } from './dto/actror.dto';
 
 @Injectable()
 export class ActorService {
   constructor(
-    @InjectModel(ActorModel) private readonly GenreModel: ModelType<ActorModel>
+    @InjectModel(ActorModel) private readonly ActorModel: ModelType<ActorModel>
   ) {}
+  async byId(_id: string) {
+    const actor = await this.ActorModel.findById(_id);
+    if (!actor) throw new NotFoundException('Actor not found');
+    return actor;
+  }
+  async bySlug(slug: string) {
+    return this.ActorModel.findOne({ slug }).exec();
+  }
+
+  async update(_id: string, dto: ActorDto) {
+    const updateActor = await this.ActorModel.findByIdAndUpdate(_id, dto, {
+      new: true,
+    }).exec();
+    if (!updateActor) {
+      throw new NotFoundException('Actor not found');
+    }
+    return updateActor;
+  }
+
+  async create() {
+    const defaultValue: ActorDto = {
+      name: '',
+      slug: '',
+      photo: '',
+    };
+    const actor = await this.ActorModel.create(defaultValue);
+    return actor._id;
+  }
+
+  async getAll(searchParam?: string) {
+    let options = {};
+    if (searchParam) {
+      options = {
+        $or: [
+          {
+            name: new RegExp(searchParam, 'i'),
+          },
+          {
+            slug: new RegExp(searchParam, 'i'),
+          },
+        ],
+      };
+    }
+    return this.ActorModel.find(options)
+      .select(' -updatedAt -__v')
+      .sort({
+        createdAt: 'desc',
+      })
+      .exec();
+  }
+
+  async delete(id: string) {
+    const deleteActor = await this.ActorModel.findByIdAndDelete(id).exec();
+    if (!deleteActor) {
+      throw new NotFoundException('actor not found');
+    }
+    return deleteActor;
+  }
 }
