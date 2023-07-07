d57e8dd757f4443324193fc947adb84056159e17
Ivan <foxriver660@gmail.com>
2023-07-07 12:31:30 +0300

    feat: completed file upload

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,7 +1,29 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Query,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Expression } from 'mongoose';
+import { Auth } from 'src/auth/decorators/auth.decorator';
 import { FileService } from './file.service';
 
 @Controller('files')
 export class FileController {
   constructor(private readonly fileService: FileService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.OK)
+  @Auth('admin')
+  @UseInterceptors(FileInterceptor('image'))
+  async upload(
+    @UploadedFile() files: Express.Multer.File,
+    @Query('folder') folder?: string
+  ) {
+    return this.fileService.saveFiles([files], (folder = 'default'));
+  }
 }
