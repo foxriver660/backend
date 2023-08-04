7e2cce54a234ed8d6ae5869b861ede755a71c586
foxriver <foxriver660@gmail.com>
2023-08-04 13:24:22 +0300

    feat: create movie/model, dto

--- src/movie/movie.module.ts
+++ src/movie/movie.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { MovieController } from './movie.controller';
 import { MovieService } from './movie.service';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { MovieModel } from './movie.model';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   controllers: [MovieController],
-  providers: [MovieService]
+  providers: [MovieService],
+  imports: [
+    TypegooseModule.forFeature([
+      {
+        typegooseClass: MovieModel,
+        schemaOptions: { collection: 'Movie' },
+      },
+    ]),
+  ],
 })
 export class MovieModule {}
