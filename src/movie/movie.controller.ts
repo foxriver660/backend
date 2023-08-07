776f5407ba42ef7c201c54dd29b7b3cdc8e35384
foxriver <foxriver660@gmail.com>
2023-08-07 19:12:28 +0300

    feat: completed movie contr.

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,4 +1,81 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { MovieService } from './movie.service';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { IdValidationPipe } from 'src/pipes/id.validation.pipe';
+import { Types } from 'mongoose';
+import { CreateMovieDto } from './dto/create-movie.dto';
 
 @Controller('movie')
-export class MovieController {}
+export class MovieController {
+  constructor(private readonly movieService: MovieService) {}
+
+  @UsePipes(new ValidationPipe())
+  @Post()
+  @HttpCode(HttpStatus.OK)
+  @Auth()
+  async createMovie() {
+    return this.movieService.create();
+  }
+
+  @Get('/by-slug/:slug')
+  async bySlug(@Param('slug') slug: string) {
+    return this.movieService.bySlug(slug);
+  }
+  @Get('/by-actor/:actorId')
+  async getByActorId(@Param('actorId') actorId: Types.ObjectId) {
+    return this.movieService.byActor(actorId);
+  }
+  @Post('/by-genres')
+  async getByGenres(@Body('genresIds') genresIds: Types.ObjectId[]) {
+    return this.movieService.byGenres(genresIds);
+  }
+  @Get()
+  async getAll(@Query('searchParam') searchParam?: string) {
+    return this.movieService.getAll(searchParam);
+  }
+  @Get('/most-popular')
+  async getMostPopular() {
+    return this.movieService.getMostPopular();
+  }
+
+  @Post('/update-count-opened')
+  @HttpCode(HttpStatus.OK)
+  async updateCountOpened(@Body('slug') slug: string) {
+    return this.movieService.updateCountOpened(slug);
+  }
+
+  @Get(':id')
+  @Auth('admin')
+  async getMovie(@Param('id', IdValidationPipe) _id: string) {
+    return this.movieService.byId(_id);
+  }
+  @Delete(':id')
+  @HttpCode(HttpStatus.OK)
+  @Auth('admin')
+  async deleteMovie(@Param('id', IdValidationPipe) _id: string) {
+    return this.movieService.delete(_id);
+  }
+  @UsePipes(new ValidationPipe())
+  @Put(':id')
+  @HttpCode(HttpStatus.OK)
+  @Auth('admin')
+  async updateGenre(
+    @Param('id', IdValidationPipe) _id: string,
+    @Body() dto: CreateMovieDto
+  ) {
+    return this.movieService.update(_id, dto);
+  }
+}
